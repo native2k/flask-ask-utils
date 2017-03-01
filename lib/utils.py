7300e950a46be44340bc3883fcbfdcab718a8537
Sven <native2k@gmail.com>
2017-03-01 12:36:57 +0100

    make loglevel flexible

--- lib/utils.py
+++ lib/utils.py
@@ -7,37 +7,39 @@ from inspect import getargvalues, stack
 
 log = logging.getLogger('utils')
 
+logLevel = logging.INFO
+
 
 def logArgvalues():
-    if log.isEnabledFor(logging.DEBUG):
+    if log.isEnabledFor(logLevel):
         posname, kwname, args = getargvalues(stack()[1][0])[-3:]
         posargs = args.pop(posname, [])
         args.update(args.pop(kwname, []))
 
-        log.debug('===== FUNCTION -> [%s]' % (stack()[1][3], ))
-        log.debug('Args: %s - %s' % (pformat(posargs), args))
-        log.debug('session: %s' % (session.get('sessionId'), ))
-        log.debug('user: %s' % (session.get('user', {}).get('userId'), ))
-        log.debug('attributes: %s' % (pformat(session.attributes), ))
+        log.log(logLevel, '===== FUNCTION -> [%s]' % (stack()[1][3], ))
+        log.log(logLevel, 'Args: %s - %s' % (pformat(posargs), args))
+        log.log(logLevel, 'session: %s' % (session.get('sessionId'), ))
+        log.log(logLevel, 'user: %s' % (session.get('user', {}).get('userId'), ))
+        log.log(logLevel, 'attributes: %s' % (pformat(session.attributes), ))
 
 
 def doReprompt(text, reprompt='', joinText=True):
     if reprompt:
-        log.debug('>>  "%s"  <<' % text)
+        log.log(logLevel, '>>  "%s"  <<' % text)
         if joinText:
             text = " ".join([text, reprompt])
     else:
         reprompt = text
-    log.debug('>>> "%s" <<<' % reprompt)
+    log.log(logLevel, '>>> "%s" <<<' % reprompt)
     return question(text.encode('utf-8')).reprompt(reprompt.encode('utf-8'))
 
 
 def doQuestion(text):
-    log.debug('>> "%s" <<' % text)
+    log.log(logLevel, '>> "%s" <<' % text)
     return question(text.encode('utf-8'))
 
 
 def doSay(text):
-    log.debug('>>! "%s" !<<' % text)
+    log.log(logLevel, '>>! "%s" !<<' % text)
     # return question(text.encode('utf-8'))
     return statement(text.encode('utf-8'))
